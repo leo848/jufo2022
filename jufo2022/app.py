642d102b890a97d1d23083454dfff5ab932ac7f1
leo848 <leonardo.blume@gymnasium-essen-werden.de>
2021-11-16 15:55:11 +0100

    commit with access

--- jufo2022/app.py
+++ jufo2022/app.py
@@ -1,7 +1,7 @@
 import secrets
 
 from flask import Flask
-
+from flask import request
 
 def generate_session() -> str:
 	return secrets.token_urlsafe(32)
@@ -15,9 +15,11 @@ def test() -> str:
 	return generate_session()
 
 
-@app.route("/access/<token>")
-def access(token: str):
-	return "token take it or leave it"
+@app.route("/access")
+def access():
+	token = request.args.get('token')
+	
+	return f'token: {token} take it or leave it'
 
 
 if __name__ == "__main__":
